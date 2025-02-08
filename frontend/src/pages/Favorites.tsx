15d915960e8d1d8520ed1cecc26982f10e9b3344
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-08 17:23:13 +0530

    Fix favorites page bug

--- frontend/src/pages/Favorites.tsx
+++ frontend/src/pages/Favorites.tsx
@@ -1,11 +1,12 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import { RootLayout } from '../layouts/RootLayout'
 import { CardContainer } from '../components/CardContainer/CardContainer'
 
 export const Favorites: FC = () => {
+  const cardContainer = useRef<HTMLDivElement>(null);
   return (
-    <RootLayout>
-      <CardContainer favoritesOnly={true} />
+    <RootLayout cardContainer={cardContainer}>
+      <CardContainer innerRef={cardContainer} favoritesOnly={true} />
     </RootLayout>
   )
 } 
