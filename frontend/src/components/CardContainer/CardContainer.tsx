887a6035ebc9e264897185109d73ee9cb27337f4
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-08 17:23:29 +0530

    Add temporary keys

--- frontend/src/components/CardContainer/CardContainer.tsx
+++ frontend/src/components/CardContainer/CardContainer.tsx
@@ -12,11 +12,11 @@ export const CardContainer: FC<CardContainerProps> = ({ innerRef, favoritesOnly 
         <div ref={innerRef} className="card-container">
             {!favoritesOnly ? (
                 Array.from({ length: 8 }, (_, i) => (
-                    <Card duration={i < 4 ? null : "00:14"} /> // sample cards
+                    <Card key={i} duration={i < 4 ? null : "00:14"} /> // sample cards
                 ))
             ) : (
-                Array.from({ length: 3 }, () => (
-                    <Card duration={"00:14"} /> // sample cards
+                Array.from({ length: 3 }, (_, i) => (
+                    <Card key={8+i} duration={"00:14"} /> // sample cards
                 ))
             )}
         </div>
