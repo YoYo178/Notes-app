8e70d35f8dc190d487f3144e0053f9d265df47e2
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-13 21:53:27 +0530

    Remove non-existent import from db.ts

--- backend/src/common/db.ts
+++ backend/src/common/db.ts
@@ -1,17 +1,17 @@
-import { log } from "@src/util/logUtils";
 import mongoose from "mongoose";
-
+import logger from 'jet-logger'
 
 export async function connectDB() {
     try {
         const mongoURI = process.env.MONGODB_URI
         if (!mongoURI) {
-            log("error", "MongoDB", null, "MONGODB_URI is not defined in the .env file!")
+            logger.err("MONGODB_URI is not defined in the .env file!")
             return;
         }
 
         await mongoose.connect(mongoURI)
     } catch (error) {
-        log("error", "MongoDB", error, "An error occured while connecting to MongoDB! More details below:")
+        logger.err("An error occured while connecting to MongoDB! More details below:")
+        logger.err(error);
     }
 }
