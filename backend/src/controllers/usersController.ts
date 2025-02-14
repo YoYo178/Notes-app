a1d4a6cf1634b0ae78c0d0a6a9545db05a4cdb93
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-14 19:58:08 +0530

    Create users controller

---
+++ backend/src/controllers/usersController.ts
@@ -0,0 +1,120 @@
+import { User } from "@src/models/User";
+import expressAsyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import bcrypt from 'bcrypt'
+import HttpStatusCodes from "@src/common/HttpStatusCodes";
+import { ObjectId } from "mongoose";
+
+/**
+@route GET /users
+@description Returns all users.
+*/
+const getAllUsers = expressAsyncHandler(async (req: Request, res: Response) => {
+    const users = await User.find().select('-password').lean().exec() || [];
+
+    if (!users.length) {
+        res.status(HttpStatusCodes.NOT_FOUND).send({ message: "No users found!" });
+        return;
+    }
+    res.status(200).send(users)
+})
+
+/**
+@route POST /users
+@description Creates a new user.
+*/
+const createUser = expressAsyncHandler(async (req: Request, res: Response) => {
+    const { username, password, displayName } = req.body;
+
+    if (!username || !password || !displayName) {
+        res.status(HttpStatusCodes.BAD_REQUEST).send({ message: "All fields are required" })
+        return;
+    }
+
+    const userExists = !!await User.findOne({ username }).lean().exec()
+
+    if (userExists) {
+        res.status(HttpStatusCodes.CONFLICT).send({ message: "A user already exists with the provided username" })
+        return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        username,
+        password: hashedPassword,
+        displayName
+    });
+
+    if (user) {
+        res.status(HttpStatusCodes.CREATED).send({ message: `User created successfully`, id: (user._id as ObjectId).toString() })
+        return;
+    } else {
+        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({ message: "An error occured while creating a new user." })
+    }
+})
+
+/**
+@route PATCH /users
+@description Updates an existing user.
+*/
+const updateUser = expressAsyncHandler(async (req: Request, res: Response) => {
+    const { id, username, password, displayName } = req.body;
+
+    if (!id || !username || !password || !displayName) {
+        res.status(HttpStatusCodes.BAD_REQUEST).send({ message: "All fields are required" });
+        return;
+    }
+
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+        res.status(HttpStatusCodes.NOT_FOUND).send({ message: "User not found" });
+        return;
+    }
+
+    const duplicateUser = await User.findOne({ username })
+    if (duplicateUser && (duplicateUser._id as ObjectId).toString() !== id) {
+        res.status(HttpStatusCodes.CONFLICT).send({ message: "Username already exists" });
+        return;
+    }
+
+    user.username = username;
+    user.displayName = displayName;
+    user.password = await bcrypt.hash(password, 10);
+
+    await user.save();
+
+    res.status(HttpStatusCodes.OK).send({ message: "User updated successfully" })
+})
+
+/**
+@route DELETE /users
+@description Deletes a user.
+*/
+const deleteUser = expressAsyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.body;
+
+    if (!id) {
+        res.status(HttpStatusCodes.BAD_REQUEST).send({ message: "User ID is required" })
+        return;
+    }
+
+    const user = await User.findById(id);
+
+    if (!user) {
+        res.status(HttpStatusCodes.NOT_FOUND).send({ message: "User not found" });
+        return;
+    }
+
+    const result = await user.deleteOne();
+
+    res.status(HttpStatusCodes.OK).send({ message: "User deleted successfully" });
+})
+
+export default {
+    getAllUsers,
+    createUser,
+    updateUser,
+    deleteUser
+}
--- backend/src/routes/Users/UsersRouter.ts
+++ backend/src/routes/Users/UsersRouter.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import usersController from '@src/controllers/usersController';
+
+const { getAllUsers, createUser, updateUser, deleteUser } = usersController;
 
 const UsersRouter = Router();
+
+UsersRouter.route('/')
+    .get(getAllUsers)
+    .post(createUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 export default UsersRouter;
