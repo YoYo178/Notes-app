bb97b1169e8264031e73bf26d8965259cb50e41b
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-15 12:16:47 +0530

    Add generic .env file support

--- backend/config.ts
+++ backend/config.ts
@@ -4,11 +4,18 @@ import path from 'path';
 import dotenv from 'dotenv';
 import moduleAlias from 'module-alias';
 
+// Import generic .env first
+const result1 = dotenv.config({
+  path: path.join(__dirname, `./config/.env`),
+});
+if (result1.error) {
+  throw result1.error
+}
 
 // Check the env
 const NODE_ENV = (process.env.NODE_ENV ?? 'development');
 
-// Configure "dotenv"
+// Import environment specific .env after importing generic .env
 const result2 = dotenv.config({
   path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
 });
