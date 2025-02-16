d5b8a8c1e31ad5593dc9afb8393c031bbb43f1b6
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-16 11:59:37 +0530

    Added Auth validator for Update user and Delete user

--- backend/src/routes/Users/UsersRouter.ts
+++ backend/src/routes/Users/UsersRouter.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import usersController from '@src/controllers/usersController';
+import AuthValidator from '@src/middleware/AuthValidator';
 
 const { createUser, updateUser, deleteUser } = usersController;
 
 const UsersRouter = Router();
 
+UsersRouter.post('/', createUser)
+
+UsersRouter.use(AuthValidator);
+
 UsersRouter.route('/')
-    .post(createUser)
     .patch(updateUser)
     .delete(deleteUser);
 
