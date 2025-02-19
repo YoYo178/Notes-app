c67f0fd122ad6ac7d53eef22f7412f58d8a0a293
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-19 20:31:25 +0530

    Create tokenConfig.ts

---
+++ backend/src/config/tokenConfig.ts
@@ -0,0 +1,8 @@
+export const tokenConfig = {
+    accessToken: {
+        expiry: 3 * 60 * 60 * 1000, // 3 hours
+    },
+    refreshToken: {
+        expiry: 7 * 24 * 60 * 60 * 1000, // 7 days
+    }
+};
