24a5bc4e714a6e3e7f4a03e3534de2912be08c0b
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-19 20:36:27 +0530

    Create authUtils.ts

---
+++ backend/src/util/authUtils.ts
@@ -0,0 +1,27 @@
+import { tokenConfig } from '@src/config/tokenConfig';
+import { IUser } from '@src/models/User';
+import jwt from 'jsonwebtoken';
+import { Document, ObjectId } from 'mongoose';
+
+/**
+ * @description Generates and returns a new access token for a user
+ * @param user User's MongoDB object
+ * @param req Request object from express
+ * @param res Response object from express
+ * @returns string
+ */
+export function refreshAccessToken(user: IUser & Document) {
+    const accessToken = jwt.sign(
+        {
+            User: {
+                id: (user._id as ObjectId).toString(),
+                username: user.username,
+                displayName: user.displayName,
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET as string,
+        { expiresIn: tokenConfig.accessToken.expiry }
+    );
+
+    return accessToken;
+}
