1feb1bc229db07accebb4839a7c5d286c6e2e181
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-28 19:33:14 +0530

    Update AuthProvider.tsx

--- frontend/src/contexts/AuthProvider.tsx
+++ frontend/src/contexts/AuthProvider.tsx
@@ -1,13 +1,24 @@
-import { createContext, FC, ReactNode, useState } from "react";
-
-const AuthContext = createContext({})
+import { createContext, FC, ReactNode, useEffect, useState } from "react";
 
 interface AuthProviderProps {
-    children: ReactNode
+    children: ReactNode;
 }
 
+interface AuthObject {
+    displayName: string | undefined;
+    username: string | undefined;
+    id: string | undefined;
+}
+
+interface AuthValues {
+    auth: {} | null;
+    setAuth: React.Dispatch<React.SetStateAction<AuthObject | null>> | null;
+}
+
+const AuthContext = createContext<AuthValues>({ auth: null, setAuth: null })
+
 export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
-    const [auth, setAuth] = useState({})
+    const [auth, setAuth] = useState<AuthObject | null>(null)
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
