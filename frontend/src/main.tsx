7b0402825cfb98f301a44075fa2415c04bcd171b
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-02-28 18:12:33 +0530

    Add QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Favorites } from './pages/Favorites/Favorites';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Outlet />}>
-          <Route index element={<Home />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Outlet />}>
+            <Route index element={<Home />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
