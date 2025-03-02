8a88f13400f1f36943ba4a946dd29a36512dbce9
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-02 21:25:15 +0530

    Update CardContainer to display notes from backend

--- frontend/src/components/CardContainer/CardContainer.tsx
+++ frontend/src/components/CardContainer/CardContainer.tsx
@@ -1,4 +1,7 @@
-import { FC, RefObject } from 'react'
+import { FC, RefObject, useEffect, useState } from 'react'
+
+import { useGetNotes } from '../../hooks/network/note/useGetNotes';
+import { Note } from '../../types/NoteTypes';
 
 import { Card } from './Card/Card';
 
@@ -9,32 +12,65 @@ interface CardContainerProps {
     favoritesOnly: boolean;
 }
 
+// Special type just to add rawDate, to sort by date
+interface MNote extends Note { rawDate: Date };
+
 export const CardContainer: FC<CardContainerProps> = ({ innerRef, favoritesOnly }) => {
+    const [notes, setNotes] = useState<MNote[]>([]);
+
+    const useGetNotesMutation = useGetNotes();
+
+    useEffect(() => {
+        console.log(notes)
+        if (!notes.length && useGetNotesMutation.isIdle) {
+            useGetNotesMutation.mutate({});
+        }
+
+        if (!notes.length && useGetNotesMutation.isSuccess && useGetNotesMutation.data) {
+            const { data: { notes: serverNotes } } = useGetNotesMutation; // 3-Layer destructured properties
+
+            if (!serverNotes || !serverNotes.length)
+                return;
+
+            const notesArr: MNote[] = []
+
+            for (const note of serverNotes) {
+                const date = new Date(note.createdAt);
+
+                const stringDate = `${date.toString().substring(4, 10)}, ${date.toString().substring(11, 15)}`;
+                const stringTime = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+
+                notesArr.push({
+                    title: note.title,
+                    description: note.description,
+                    isText: note.isText,
+                    duration: note.duration,
+                    date: `${stringDate} · ${stringTime}`,
+                    rawDate: date
+                })
+            }
+
+            // Sort in descending order (Newest first)
+            notesArr.sort((a, b) => b.rawDate.valueOf() - a.rawDate.valueOf())
+
+            setNotes(notesArr);
+        }
+    }, [notes.length, useGetNotesMutation.isSuccess])
+
     return (
         <div ref={innerRef} className="card-container">
-            {!favoritesOnly ? (
-                Array.from({ length: 8 }, (_, i) => (
+            {notes.map((note, i) => {
+                return (
                     <Card
                         key={i}
-                        date="Jan 30, 2025 · 5:26 PM"
-                        duration={i < 4 ? null : "00:14"}
-                        isText={i < 4}
-                        title="Engineering Assignment Audio"
-                        description="I'm recording an audio to transcribe into text for the assignment of engineering in terms of actors."
-                    /> // sample cards
-                ))
-            ) : (
-                Array.from({ length: 3 }, (_, i) => (
-                    <Card
-                        key={8 + i}
-                        date="Jan 30, 2025 · 5:26 PM"
-                        duration="00:14"
-                        isText={false}
-                        title="Engineering Assignment Audio"
-                        description="I'm recording an audio to transcribe into text for the assignment of engineering in terms of actors."
-                    /> // sample cards
-                ))
-            )}
+                        title={note.title}
+                        description={note.description}
+                        date={note.date}
+                        duration={note.duration}
+                        isText={note.isText}
+                    />
+                )
+            })}
         </div>
     )
 }
