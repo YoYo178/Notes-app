a3e8c9392a8f57a3ef520a026580a1f69a0f2916
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-02 11:08:02 +0530

    Create Register page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { AuthProvider } from './contexts/AuthProvider';
+
 import { Favorites } from './pages/Favorites/Favorites';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
-import { AuthProvider } from './contexts/AuthProvider';
+import { Register } from './pages/Register/Register'
 
 const queryClient = new QueryClient();
 
@@ -20,7 +22,7 @@ createRoot(document.getElementById('root')!).render(
               <Route index element={<Home />} />
               <Route path="favorites" element={<Favorites />} />
               <Route path="login" element={<Login />} />
-              <Route path='register' element={<h1>TODO: Register page</h1>} />
+              <Route path='register' element={<Register />} />
               <Route path='reset' element={<h1>TODO: Reset password page</h1>} />
 
               <Route path="*" element={<h1>TODO: Not found page</h1>} />
---
+++ frontend/src/pages/Register/Register.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import './Register.css'
+
+export const Register: FC = () => {
+    return (
+        <div className="register-form-container">
+        </div>
+    )
+}
