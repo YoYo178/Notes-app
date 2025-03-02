66a3309ee80a70fba55770fc9791a4cb4b0692ba
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-02 21:21:49 +0530

    Add Note and User endpoints

--- frontend/src/config/APIEndpoints.ts
+++ frontend/src/config/APIEndpoints.ts
@@ -1,6 +1,8 @@
 import { Endpoints } from "../types/APITypes";
 
 const APIEndpoints: Endpoints = {
+
+    /***** AUTHENTICATION *****/
     LOGIN: {
         URL: "/api/auth/login",
         METHOD: "POST"
@@ -20,7 +22,35 @@ const APIEndpoints: Endpoints = {
     AUTH_QUERY: {
         URL: "/api/auth/",
         METHOD: "GET"
-    }
+    },
+
+    /***** NOTE *****/
+    GET_ALL_NOTES: {
+        URL: "/api/notes",
+        METHOD: "GET"
+    },
+    CREATE_NOTE: {
+        URL: "/api/notes",
+        METHOD: "POST"
+    },
+    UPDATE_NOTE: {
+        URL: "/api/notes",
+        METHOD: "PATCH"
+    },
+    DELETE_NOTE: {
+        URL: "/api/notes",
+        METHOD: "DELETE"
+    },
+
+    /***** USER *****/
+    UPDATE_USER: {
+        URL: "/api/users",
+        METHOD: "PATCH"
+    },
+    DELETE_USER: {
+        URL: "/api/users",
+        METHOD: "DELETE"
+    },
 }
 
 export default APIEndpoints;
