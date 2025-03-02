32e8d6c5ea58873db22595bb9f0c77c7b0a5ff7a
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-02 17:40:11 +0530

    Update sameSite to use Lax instead of None

--- backend/src/config/cookieConfig.ts
+++ backend/src/config/cookieConfig.ts
@@ -4,7 +4,7 @@ import { tokenConfig } from "./tokenConfig";
 const cookieConfig: CookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    sameSite: "lax",
     maxAge: tokenConfig.accessToken.expiry // Re-used access token's expiry, 3 hours by default
 };
 
