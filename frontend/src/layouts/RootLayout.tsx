7de1e37c20f61cf2f4fefe370ae15f8c26ec9433
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-02 17:44:30 +0530

    Fetch auth status on initial render

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,4 +1,7 @@
-import { FC, RefObject } from 'react'
+import { FC, RefObject, useContext, useEffect } from 'react'
+
+import { useAuthQuery } from '../hooks/auth/useAuthQuery'
+import AuthContext from '../contexts/AuthProvider'
 
 import { CreateNoteBar } from '../components/CreateNoteBar/CreateNoteBar'
 import { SearchBox } from '../components/SearchBox/SearchBox'
@@ -13,6 +16,21 @@ interface RootLayoutProps {
 }
 
 export const RootLayout: FC<RootLayoutProps> = ({ cardContainer, children }) => {
+  const { auth, setAuth } = useContext(AuthContext);
+  const authStatusMutation = useAuthQuery();
+
+  useEffect(() => {
+    if (!auth && authStatusMutation.isIdle) {
+      authStatusMutation.mutate({});
+    }
+
+    if (!auth && !!setAuth && authStatusMutation.isSuccess && authStatusMutation.data) {
+      const { data: { user: { id, username, displayName } } } = authStatusMutation; // 3-Layer destructured properties
+
+      setAuth({ id, username, displayName });
+    }
+  }, [auth, authStatusMutation.isSuccess])
+
   return (
     <div className="app-container">
       <Sidebar />
