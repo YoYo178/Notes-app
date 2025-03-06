f531d31cdcbdf6e241e778111c5fd856115930f7
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-06 21:22:32 +0530

    Create useQueryBase hook

---
+++ frontend/src/hooks/network/useQueryBase.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import API from '../../api/backendAPI'
+import { HTTP_METHODS } from "../../types/APITypes";
+import { AxiosError } from "axios";
+
+export function useQueryBase(endpoint: { URL: string, METHOD: HTTP_METHODS }, queryKey: string[], sendCookies: boolean = false, shouldRetry: boolean | ((failureCount: number, error: any) => boolean) = false, staleTime: number | undefined = undefined) {
+    return () => useQuery({
+        queryKey,
+        queryFn: async () => {
+            const { data } = await API.get(endpoint.URL, { withCredentials: sendCookies })
+            return data;
+        },
+        retry: (failureCount: number, error: AxiosError) => {
+            if (typeof shouldRetry === 'function')
+                return shouldRetry(failureCount, error);
+
+            if (error.status === 401)
+                return false;
+
+            return shouldRetry;
+        },
+        staleTime
+    })
+}
