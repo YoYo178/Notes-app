6271ba97c1f69c480677841152a1c072c2da6a46
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-03-14 13:36:07 +0530

    Create EditNoteModal

---
+++ frontend/src/components/CardContainer/Card/EditNoteModal/EditNoteModal.ts
@@ -0,0 +1,28 @@
+import { UseMutationResult } from "@tanstack/react-query";
+import { Note } from "../../../../types/NoteTypes";
+
+function saveNoteOnClick(
+    createNoteMutation: UseMutationResult<any, Error, Partial<Note> | undefined, unknown>,
+    fields: Partial<Note>
+) {
+    const { id, title, description, isText, isFavorite, images, duration } = fields;
+
+    if (!title || !description) {
+        console.error("Title or description fields cannot be empty!")
+        return;
+    }
+
+    createNoteMutation.mutate({
+        id,
+        title,
+        description,
+        isText,
+        duration,
+        isFavorite,
+        images
+    })
+}
+
+export const ButtonHandler = {
+    saveNoteOnClick
+}
---
+++ frontend/src/components/CardContainer/Card/EditNoteModal/EditNoteModal.tsx
@@ -0,0 +1,61 @@
+import { FC, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom';
+
+import { FaCheck } from 'react-icons/fa';
+import { IoMdClose } from 'react-icons/io';
+
+import { useUpdateNoteMutation } from '../../../../hooks/network/note/useUpdateNoteMutation';
+
+import { ButtonHandler } from './EditNoteModal';
+
+import './EditNoteModal.css'
+
+interface CreateNoteModelProps {
+    isOpen: boolean;
+    onClose: () => void;
+    originalTitle: string;
+    originalDescription: string;
+    noteID: string;
+}
+
+export const EditNoteModal: FC<CreateNoteModelProps> = ({ isOpen, onClose, originalTitle, originalDescription, noteID }) => {
+    const [title, setTitle] = useState(originalTitle);
+    const [description, setDescription] = useState(originalDescription);
+
+    const updateNoteMutation = useUpdateNoteMutation();
+
+    useEffect(() => {
+        if (isOpen && !updateNoteMutation.error) {
+            onClose();
+        }
+    }, [updateNoteMutation.isSuccess])
+
+    if (!isOpen) return null;
+
+    return createPortal(
+        <div className='enm-backdrop' onMouseDown={onClose}>
+            <div className='enm' onMouseDown={(e) => e.stopPropagation()}>
+                <div className="enm-header">
+                    <h2 className='enm-title'>Edit Note</h2>
+                    <button className='enm-close-button' onClick={onClose}>
+                        <IoMdClose className='enm-close-button-icon' />
+                    </button>
+                </div>
+                <div className="enm-fields">
+                    <div className="enm-text-field-container">
+                        <input type="text" className="enm-field-title" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+                    <div className="enm-text-field-container enm-description-field-container">
+                        <textarea className="enm-field-description" placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} />
+                    </div>
+                </div>
+                <div className="enm-footer">
+                    <button className="enm-check-button" onClick={() => ButtonHandler.saveNoteOnClick(updateNoteMutation, { title, description, isText: true, duration: null, id: noteID })}>
+                        <FaCheck />
+                    </button>
+                </div>
+            </div>
+        </div>,
+        document.getElementById("modal-root")!
+    )
+}
