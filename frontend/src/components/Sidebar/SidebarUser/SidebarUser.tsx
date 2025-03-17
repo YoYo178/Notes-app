0a84641db53360210db4313447f83bbb549d6946
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-03-17 22:22:24 +0530

    Close dropdown menu upon clicking outside

--- frontend/src/components/Sidebar/SidebarUser/SidebarUser.tsx
+++ frontend/src/components/Sidebar/SidebarUser/SidebarUser.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { IoMdArrowDropdown } from "react-icons/io";
@@ -24,18 +24,39 @@ const SidebarUser: FC<SidebarUserProps> = ({ displayName }) => {
     const [angle, setAngle] = useState(0);
     const [isDropdownMenuVisible, setIsDropdownMenuVisible] = useState(angle === 180);
 
+    const sidebarItemsRef = useRef<HTMLDivElement>(null);
+
     const logoutMutation = useLogoutMutation();
 
     useEffect(() => {
         if (logoutMutation.isSuccess)
             window.location.reload();
 
-    }, [logoutMutation.isSuccess])
+    }, [logoutMutation.isSuccess]);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (sidebarItemsRef.current && !sidebarItemsRef.current.contains(event.target as Node)) {
+                setIsDropdownMenuVisible(false);
+                setAngle(0); // Reset arrow angle
+            }
+        };
+
+        if (isDropdownMenuVisible) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isDropdownMenuVisible]);
 
     return (
         <div className="sidebar-user">
             <IoPersonCircleSharp className="sidebar-user-profile-photo" />
-            <div className="sidebar-user-items">
+            <div ref={sidebarItemsRef} className="sidebar-user-items">
                 <p className="sidebar-user-name" onClick={() => ButtonHandler.dropdownButtonOnClick(setAngle, angle, setIsDropdownMenuVisible)}>
                     {displayName}
                 </p>
