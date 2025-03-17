6cac13ec2d7dabb9cc6196f255a2b29e9685c9d7
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-03-17 22:11:20 +0530

    Add staleTime to useGetNotesQuery
    This change was done to prevent unnecessary re-fetching of notes on page navigation.

--- frontend/src/hooks/network/note/useGetNotesQuery.ts
+++ frontend/src/hooks/network/note/useGetNotesQuery.ts
@@ -1,4 +1,4 @@
 import { useQueryBase } from "../useQueryBase";
 import APIEndpoints from "../../../config/APIEndpoints";
 
-export const useGetNotesQuery = useQueryBase(APIEndpoints.GET_ALL_NOTES, ['notes'], true, true);
+export const useGetNotesQuery = useQueryBase(APIEndpoints.GET_ALL_NOTES, ['notes'], true, true, 5 * 60 * 1000);
