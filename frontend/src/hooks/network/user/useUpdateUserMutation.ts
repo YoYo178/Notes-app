220ca04e500aa1a75a3045aefd857ad88ee40b23
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-22 17:06:53 +0530

    Create useUpdateUserMutation hook

---
+++ frontend/src/hooks/network/user/useUpdateUserMutation.ts
@@ -0,0 +1,5 @@
+import { useMutationBase } from '../useMutationBase';
+import { UserUpdateFields } from '../../../types/user.types';
+import APIEndpoints from "../../../config/APIEndpoints";
+
+export const useUpdateUserMutation = useMutationBase<Partial<UserUpdateFields>>(APIEndpoints.UPDATE_USER, ['auth'], "Update user", true);
