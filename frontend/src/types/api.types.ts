c4b61f9b6d5679a85ba61f69b3f125a3d57c3982
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-22 17:05:50 +0530

    Improve API types

--- frontend/src/types/api.types.ts
+++ frontend/src/types/api.types.ts
@@ -1,8 +1,16 @@
 export type HTTP_METHODS = "GET" | "PATCH" | "PUT" | "POST" | "DELETE" | "OPTIONS";
 
-export interface Endpoints {
-    [key: string]: {
-        URL: string;
-        METHOD: HTTP_METHODS;
-    }
+type AUTH_ROUTES = "LOGIN" | "LOGOUT" | "REFRESH" | "REGISTER" | "AUTH_QUERY";
+type NOTE_ROUTES = "GET_ALL_NOTES" | "CREATE_NOTE" | "UPDATE_NOTE" | "DELETE_NOTE";
+type USER_ROUTES = "UPDATE_USER" | "DELETE_USER";
+
+type API_ROUTES = AUTH_ROUTES | NOTE_ROUTES | USER_ROUTES;
+
+export type Endpoint = {
+    URL: string;
+    METHOD: HTTP_METHODS;
+}
+
+export type Endpoints = {
+    [key in API_ROUTES]: Endpoint
 }
