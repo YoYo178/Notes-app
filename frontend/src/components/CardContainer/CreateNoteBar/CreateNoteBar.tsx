92bd310b25233c1906b21167543d8e226acccf61
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-29 21:40:30 +0530

    Adjust HTML structure and styles

--- frontend/src/components/CardContainer/CreateNoteBar/CreateNoteBar.tsx
+++ frontend/src/components/CardContainer/CreateNoteBar/CreateNoteBar.tsx
@@ -1,6 +1,5 @@
 import { FC, useContext, useEffect, useRef, useState } from 'react'
 
-import { RiImageAddLine } from 'react-icons/ri';
 import { FiFilePlus } from 'react-icons/fi';
 import { BsRecord2, BsStopFill } from 'react-icons/bs';
 
@@ -87,14 +86,9 @@ export const CreateNoteBar: FC<CreateNoteBarProps> = ({ isVisible }) => {
             )}
 
             <div className={`create-note-bar ${isVisible ? 'visible' : 'hidden'}`}>
-                <div className="add-note-buttons-container">
-                    <button className='add-note-button' onClick={() => { setNoteType('text'); setModalOpen(true) }}>
-                        <FiFilePlus className='add-note-icon' />
-                    </button>
-                    <button className='add-note-image-button'>
-                        <RiImageAddLine className='add-note-image-icon' />
-                    </button>
-                </div>
+                <button className='add-note-button' onClick={() => { setNoteType('text'); setModalOpen(true) }}>
+                    <FiFilePlus />
+                </button>
                 <div className="record-note-button-container" onClick={
                     async () => {
                         ButtonHandler.recordButtonOnClick(
