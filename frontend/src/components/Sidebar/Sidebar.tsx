a3a1940c33a79f646ecc8dc9bb07714a91eaa4f9
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-03-29 12:26:44 +0530

    Add Speech Recognition not supported warning

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import { FC, useContext } from 'react'
 
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoIosWarning } from 'react-icons/io'
 
 import AuthContext from '../../contexts/AuthProvider'
 import RecordingContext from '../../contexts/RecordingProvider.tsx'
+import TranscriptionContext from '../../contexts/TranscriptionProvider.tsx'
 
 import { MAX_AUDIO_RECORD_DURATION } from '../../types/recording.types.ts'
 
@@ -20,6 +22,7 @@ interface SidebarProps {
 }
 
 export const Sidebar: FC<SidebarProps> = ({ isSearchBoxOpen, isMediaQueryActive }) => {
+  const { transcript, hasSpeechRecognitionSupport } = useContext(TranscriptionContext)
   const { isRecording, recordingTime } = useContext(RecordingContext);
   const { auth } = useContext(AuthContext);
 
@@ -32,12 +35,24 @@ export const Sidebar: FC<SidebarProps> = ({ isSearchBoxOpen, isMediaQueryActive 
 
         {/* Recording Note Indicator */}
         {isRecording && (
-          <div className="sidebar-recording-indicator">
-            <div className="sidebar-recording-pulse"></div>
-            <span className="sidebar-recording-text">Recording... ({MAX_AUDIO_RECORD_DURATION - (recordingTime || 0)}s)</span>
+          <div className="sidebar-recording-container">
+            <div className="sidebar-recording-indicator">
+              <div className="sidebar-recording-pulse"></div>
+              <span className="sidebar-recording-text">Recording... ({MAX_AUDIO_RECORD_DURATION - (recordingTime || 0)}s)</span>
+            </div>
+            {!hasSpeechRecognitionSupport ? (
+              <div className="sidebar-live-transcript-not-supported-container">
+                <IoIosWarning className='sidebar-live-transcript-not-supported-icon' />
+                <span className="sidebar-live-transcript-not-supported">Your browser does not support Speech Recognition.</span>
+              </div>
+            ) : (
+              transcript && (
+                <div className="sidebar-live-transcript-text">{transcript}</div>
+              )
+            )}
           </div>
         )}
-        
+
         {!!auth ? (
           <SidebarUser displayName={auth?.displayName} />
         ) : (
