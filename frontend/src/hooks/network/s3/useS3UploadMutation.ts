53dbadb65161e520d21759bc1c5b4a595bf29f7a
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-06 14:51:15 +0530

    Create useS3UploadMutation hook
    Additional info: Does not use the useMutationBase hook

---
+++ frontend/src/hooks/network/s3/useS3UploadMutation.ts
@@ -0,0 +1,13 @@
+import { useMutation } from "@tanstack/react-query";
+import { API } from "../../../api/backendAPI";
+
+export const useUploadToS3Mutation = () =>
+    useMutation({
+        mutationFn: async ({ url, file }: { url: string, file: File }) => {
+            const { data } = await API.put(url, file, {
+                headers: { 'Content-Type': file.type },
+            });
+
+            return data ?? null;
+        }
+    });
