d439680bc8128c7b7300d99bed355aa904c3a8d9
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-07 17:01:13 +0530

    Update FilesRouter.ts

--- backend/src/routes/Files/FilesRouter.ts
+++ backend/src/routes/Files/FilesRouter.ts
@@ -2,15 +2,16 @@ import { Router } from 'express';
 import filesController from '../../controllers/filesController';
 import AuthValidator from '../../middleware/AuthValidator';
 
-const { getUploadURL, getURL, deleteFile } = filesController;
+const { getUploadURL, getURL, getMultipleURL, deleteFile } = filesController;
 
 const FilesRouter = Router();
 
 FilesRouter.use(AuthValidator);
 
 FilesRouter.post('/getUploadURL', getUploadURL);
+FilesRouter.post('/getMultipleURL', getMultipleURL)
 
-FilesRouter.get('/getURL/:fileKey', getURL);
+FilesRouter.get('/getURL/', getURL);
 FilesRouter.delete('/delete/:fileKey', deleteFile)
 
 export default FilesRouter;
