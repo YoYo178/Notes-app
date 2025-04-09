73454824360cc9485e52f5aeeee1c1639a6d5b04
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-09 12:51:47 +0530

    Updated useMutationBase hook
    Added support for optimistic updates.
    Added proper support for path parameters and query parameters.

--- frontend/src/hooks/network/useMutationBase.ts
+++ frontend/src/hooks/network/useMutationBase.ts
@@ -2,37 +2,63 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 import { API } from "../../api/backendAPI";
-
 import { Endpoint } from "../../types/api.types";
-import { injectPathParams } from "../../util/api.utils";
+import { injectPathParams, injectQueryParams } from "../../util/api.utils";
 
-export const useMutationBase = <T>(endpoint: Endpoint, actionName: string, sendCookies: boolean = false) => {
-    return ({ queryKey = [], pathParams = {} }: { queryKey?: string[]; pathParams?: Record<string, string>; }) => {
+export const useMutationBase = <T>(
+    endpoint: Endpoint,
+    actionName: string,
+    sendCookies: boolean = false,
+    options?: {
+        optimisticUpdate?: (variables: T, oldData: any) => any;
+    }
+) => {
+    return ({ queryKey = [] }: { queryKey?: string[]; }) => {
         const queryClient = useQueryClient();
 
         return useMutation({
-            mutationFn: async (data?: T) => {
+            mutationFn: async ({ payload, queryParams, pathParams }: { payload?: T, queryParams?: Record<string, string>, pathParams?: Record<string, string> }) => {
                 // @ts-ignore
                 const HTTPFunc = API[endpoint.METHOD.toLowerCase()];
-                const URL = injectPathParams(endpoint.URL, pathParams);
+                const URL =
+                    pathParams
+                        ? injectPathParams(endpoint.URL, pathParams)       // if pathParams
+                        : queryParams
+                            ? injectQueryParams(endpoint.URL, queryParams) // else if queryParams
+                            : endpoint.URL;                                // else
 
                 let response: AxiosResponse<any> | null = null;
 
                 if (["POST", "PUT", "PATCH"].includes(endpoint.METHOD))
-                    response = await HTTPFunc(URL, data, { withCredentials: sendCookies });
+                    response = await HTTPFunc(URL, payload, { withCredentials: sendCookies });
 
                 if (endpoint.METHOD === "DELETE")
-                    response = await HTTPFunc(URL, { withCredentials: sendCookies, data });
+                    response = await HTTPFunc(URL, { withCredentials: sendCookies, payload });
 
                 if (endpoint.METHOD === "OPTIONS")
                     response = await HTTPFunc(URL, { withCredentials: sendCookies });
 
                 return response?.data || null;
             },
-            onSuccess: () => {
-                queryClient.invalidateQueries({ queryKey });
+
+            onMutate: async (variables) => {
+                await queryClient.cancelQueries({ queryKey });
+
+                const previousData = queryClient.getQueryData(queryKey);
+
+                if (queryKey.length && options?.optimisticUpdate) {
+                    const updated = options.optimisticUpdate(variables as T, previousData);
+                    queryClient.setQueryData(queryKey, updated);
+                }
+
+                return { previousData };
             },
-            onError: (err) => {
+
+            onError: (err, _variables, context) => {
+                if (context?.previousData) {
+                    queryClient.setQueryData(queryKey, context.previousData);
+                }
+
                 if (axios.isAxiosError(err)) {
                     const error = err as AxiosError<{ message: unknown }>;
                     console.error(`${actionName} failed:`, error?.response?.data?.message);
@@ -40,6 +66,12 @@ export const useMutationBase = <T>(endpoint: Endpoint, actionName: string, sendC
                     console.error(`${actionName} failed:`, err?.message);
                 } else {
                     console.error("Unknown error occurred", err);
+                }
+            },
+
+            onSettled: (_data, _error, _variables, _context) => {
+                if (!options?.optimisticUpdate) {
+                    queryClient.invalidateQueries({ queryKey });
                 }
             },
         });
