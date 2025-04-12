9f49b349029ff379c2e404cb5abfe8b54cc7e005
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-12 23:14:08 +0530

    Create code.utils.ts

---
+++ backend/src/util/code.utils.ts
@@ -0,0 +1,8 @@
+export const VERIFICATION_CODE_TTL = 60 * 1000 // 15 minutes
+
+export function generateVerificationCode(length: number = 6) {
+    let chars = "0123456789"
+    let result = '';
+    for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
