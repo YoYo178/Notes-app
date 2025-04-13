8b40413058920f92e20e8e058780f6f582765cc9
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-13 18:05:02 +0530

    Update User model
    Added isVerified state, and recoveryState property with the following fields:
    isRecovering: True if user has initiated an account recovery.
    hasVerifiedMail: True if user has successfully verified mail for account recovery.
    hasSetPassword: True if user has completed the recovery process
    
    Once all the above fields are true, the recovery process is considered complete and all fields are set to false again.

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,18 +1,32 @@
 import { MongooseModel } from "@src/util/db.utils";
 import { Schema } from "mongoose";
 
+interface IUserRecoveryState {
+    isRecovering: boolean;
+    hasVerifiedMail: boolean;
+    hasSetPassword: boolean;
+};
+
 interface IUser {
     displayName: string;
     username: string;
     password: string;
     email: string;
+    isVerified: boolean;
+    recoveryState: IUserRecoveryState;
 }
 
 const userSchema: Schema = new Schema<IUser>({
     displayName: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String, required: true }
+    email: { type: String, required: true },
+    isVerified: { type: Boolean, required: false, default: false },
+    recoveryState: new Schema<IUserRecoveryState>({
+        isRecovering: { type: Boolean, required: false, default: false },
+        hasVerifiedMail: { type: Boolean, required: false, default: false },
+        hasSetPassword: { type: Boolean, required: false, default: false }
+    })
 })
 
 const User = MongooseModel<IUser>("users", userSchema);
