e2299cd1a2a03b379ffa5e56275d1277f5fb87fb
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-13 19:41:55 +0530

    Create useVerifyCodeMutation hook

---
+++ frontend/src/hooks/network/auth/useVerifyCodeMutation.ts
@@ -0,0 +1,13 @@
+import { useMutationBase } from "../useMutationBase";
+import APIEndpoints from "../../../config/APIEndpoints";
+
+export interface VerifyCodeRequest {
+    /** User ID */
+    id: string,
+    /** The purpose of the verification code */
+    purpose: 'user-verification' | 'reset-password',
+    /** Verification code */
+    code: string
+}
+
+export const useVerifyCodeMutation = useMutationBase<VerifyCodeRequest>(APIEndpoints.VERIFY, "Mail Verification");
