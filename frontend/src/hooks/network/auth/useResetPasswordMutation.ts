d7f56b9095d1c51d67ca53a6c527f9268372720c
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-13 19:42:13 +0530

    Create useResetPasswordMutation hook

---
+++ frontend/src/hooks/network/auth/useResetPasswordMutation.ts
@@ -0,0 +1,13 @@
+import { useMutationBase } from "../useMutationBase";
+import APIEndpoints from "../../../config/APIEndpoints";
+
+export interface ResetPasswordRequest {
+    /** User ID */
+    id: string,
+    /** New password */
+    password: string,
+    /** New password confirmation */
+    confirmPassword: string,
+}
+
+export const useResetPasswordMutation = useMutationBase<ResetPasswordRequest>(APIEndpoints.RESET_PASSWORD, "Password reset");
