4fe9be1066450fd2d223059e5dbb400181d206b6
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-13 18:05:32 +0530

    Update tokenConfig to add reset password token expiry

--- backend/src/config/tokenConfig.ts
+++ backend/src/config/tokenConfig.ts
@@ -4,5 +4,8 @@ export const tokenConfig = {
     },
     refreshToken: {
         expiry: 7 * 24 * 60 * 60 * 1000, // 7 days
+    },
+    resetPasswordAccessToken: {
+        expiry: 15 * 60 * 1000 // 15 minutes
     }
 };
