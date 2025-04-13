3a32e5aa4809f5afa65ff3e31de55dfd37e665a3
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-13 16:17:53 +0530

    Create VerificationCode model

---
+++ backend/src/models/VerificationCode.ts
@@ -0,0 +1,25 @@
+import { MongooseModel } from "@src/util/db.utils";
+import { ObjectId, Schema } from "mongoose";
+
+interface IVerificationCode {
+    user: ObjectId;
+    code: string;
+    purpose: string;
+    expiresAt: Date;
+}
+
+const codeSchema: Schema = new Schema<IVerificationCode>(
+    {
+        user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        code: { type: String, required: true },
+        purpose: { type: String, required: true, enum: ['user-verification', 'reset-password'] },
+        expiresAt: { type: Date, required: true, index: { expires: 0 } }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const VerificationCode = MongooseModel<IVerificationCode>("codes", codeSchema);
+
+export { IVerificationCode, VerificationCode }
