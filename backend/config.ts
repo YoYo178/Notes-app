aa8ef5af61539dc7bed366779cd1688bda4e1867
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-18 22:40:11 +0530

    Comment environment specific env file loading code
    jet-env seems to break with this setup.

--- backend/config.ts
+++ backend/config.ts
@@ -3,6 +3,7 @@
 import path from 'path';
 import dotenv from 'dotenv';
 import moduleAlias from 'module-alias';
+// import Env from '@src/common/Env'
 
 // Import generic .env first
 const result1 = dotenv.config({
@@ -13,15 +14,18 @@ if (result1.error) {
 }
 
 // Check the env
-const NODE_ENV = (process.env.NODE_ENV ?? 'development');
+/*
+const NODE_ENV = (Env.NodeEnv ?? 'development');
 
 // Import environment specific .env after importing generic .env
 const result2 = dotenv.config({
   path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
+  override: true
 });
 if (result2.error) {
   throw result2.error;
 }
+*/
 
 // Configure moduleAlias
 if (__filename.endsWith('js')) {
