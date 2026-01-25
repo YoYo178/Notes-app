3a9a09f5a9de86cd6c8e179b5fdae60956e60064
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-01-25 21:18:55 +0530

    Fix assets URL

--- frontend/src/utils/note.utils.ts
+++ frontend/src/utils/note.utils.ts
@@ -1,6 +1,9 @@
 import { BACKEND_URL } from "../config/backendConfig";
 
+const isProduction = import.meta.env.PROD;
+const ASSETS_PATH = isProduction ? 'notes-app/assets' : 'assets';
+
 export function getFileURL(userId: string, fileType: 'audio' | 'image', fileName: string) {
-    const url = new URL(`assets/${fileType === 'image' ? 'images' : 'audio'}/${userId}/${fileName}`, BACKEND_URL)
+    const url = new URL(`${ASSETS_PATH}/${fileType === 'image' ? 'images' : 'audio'}/${userId}/${fileName}`, BACKEND_URL)
     return url.toString();
 }
